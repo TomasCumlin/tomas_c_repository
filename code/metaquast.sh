7f9f129a637b9b32eb2428ef8ddbf3eef30bdd4a
Tomas Cumlin <tomasc@rackham2.uppmax.uu.se>
2021-04-26 16:58:59 +0200

    added metaquast script

--- code/metaquast.sh
+++ code/metaquast.sh
@@ -4,7 +4,7 @@
 #SBATCH -M snowy
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 01:00:00
+#SBATCH -t 01:20:00
 #SBATCH -J metaquast
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user tomas.cumlin.1485@student.uu.se
@@ -14,4 +14,4 @@ module load bioinfo-tools
 module load quast
 
 # Your commands
-metaquast.py /home/tomasc/genome_analysis_2021/analysis/final.contigs.fa -o /home/tomasc/genome_analysis_2021/analysis/metaquast -t 2 -1 /home/tomasc/genome_analysis_2021/tomas_c_repository/data/dna_data/*_1.paired.trimmed.fastq.gz -2 /home/tomasc/genome_analysis_2021/tomas_c_repository/data/dna_data/*_2.paired.trimmed.fastq.gz
+metaquast.py -t 2 /home/tomasc/genome_analysis_2021/analysis/final.contigs.fa
